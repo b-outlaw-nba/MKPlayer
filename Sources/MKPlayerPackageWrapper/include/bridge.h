e93dbfcb16db2ba414036b045bb2493f0910b473
Karthi K <karthi.k@mediakind.com>
2023-05-16 23:38:18 +0530

    Added SPM with hardcoded mkplayer-binary

---
+++ Sources/MKPlayerPackageWrapper/include/bridge.h
@@ -0,0 +1,19 @@
+//
+//  Header.h
+//  
+//
+//  Created by Karthi on 18/05/23.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+#ifdef __OBJC__
+
+// Add all required headers from the binary framework.
+#import <MKPlayer/MKPlayer.h>
+#endif
+
+#include <stdio.h>
+
+#endif
